d1093b120bac36b3dae2f0a28d817a2e61b5c46d
RuqayaKhalil <ruqayakamal18199@gmail.com>
2024-07-31 22:18:52 +0300

    Incomplete solution, missing bonus and other small fixes in the code

---
+++ OOPExam/src/com/mst/oop/exam/ClassRoom.java
@@ -0,0 +1,33 @@
+package com.mst.oop.exam;
+
+import java.util.Arrays;
+
+public class ClassRoom {
+	private String name;
+	private Teacher teacher;
+	private Student[] students = new Student[15];
+	
+	public ClassRoom(String name, Teacher teacher, Student[] students) {
+		this.name = name;
+		this.teacher = teacher;
+		this.students = students;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public Student[] getStudents() {
+		return students;
+	}
+
+	@Override
+	public String toString() {
+		return "ClassRoom [name=" + name + ", teacher=" + teacher + ", students=" + Arrays.toString(students) + "]";
+	}
+	
+}
